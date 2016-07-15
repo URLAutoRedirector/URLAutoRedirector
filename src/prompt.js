36d8ac5800abaa8eb7903333279a05a59fc1e13d
David Zhang <crispgm@gmail.com>
2016-07-15 10:38:56 +0800

    bugfix #44

--- src/prompt.js
+++ src/prompt.js
@@ -2,11 +2,13 @@
 // Copyright (c) David Zhang, 2016
 // Idea inspired by Albert Li.
 
-$(document).ready(function(){
-  $(".uar-prompt-layer").detach();
-  var promptMsg = chrome.i18n.getMessage("prompt_msg");
-  $("body").append("<div class=\"uar-prompt-layer\">" + promptMsg + "</div>");
-  setInterval(function(){
-  	$(".uar-prompt-layer").detach();
-  }, 3000);
-});
+if(typeof jQuery != "undefined"){
+  $(document).ready(function(){
+    $(".uar-prompt-layer").detach();
+    var promptMsg = chrome.i18n.getMessage("prompt_msg");
+    $("body").append("<div class=\"uar-prompt-layer\">" + promptMsg + "</div>");
+    setInterval(function(){
+      $(".uar-prompt-layer").detach();
+    }, 3000);
+  });
+}
