3616914c7d62ec33e48532cd3f1c21d20805b173
David Zhang <crispgm@gmail.com>
2022-06-10 11:19:10 +0800

    fix: add service worker workaround

--- src/background.js
+++ src/background.js
@@ -351,3 +351,8 @@ chrome.runtime.onInstalled.addListener(function (details) {
     });
   }
 });
+
+// Chrome service worker (inactive) workaround from: https://stackoverflow.com/questions/66618136/persistent-service-worker-in-chrome-extension
+chrome.webNavigation.onHistoryStateUpdated.addListener(function (_details) {
+  console.log('webNavigation: history status updated');
+});
